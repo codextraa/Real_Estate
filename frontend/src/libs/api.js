d02eb17555917d3f62c36d1285d58f15dee034bb
wasee-sun <revengersun@gmail.com>
2025-09-24 21:40:33 +0600

    Added config files and frontend api pages

---
+++ frontend/src/libs/api.js
@@ -0,0 +1,8 @@
+// import { ApiClient } from "./apiClient";
+// import { getRefreshTokenFromSession } from "./cookie";
+
+// const HTTPS = process.env.HTTPS === "true";
+// const API_URL = HTTPS
+//   ? process.env.API_BASE_HTTPS_URL
+//   : process.env.API_BASE_URL;
+// const apiClient = new ApiClient(API_URL);
