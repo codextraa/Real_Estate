f5496d339585bfddb76e11214226fd940a423729
wasee-sun <revengersun@gmail.com>
2025-09-29 22:39:39 +0600

    Login page styles created

--- frontend/src/components/buttons/Buttons.jsx
+++ frontend/src/components/buttons/Buttons.jsx
@@ -1,14 +1,37 @@
-"use client";
+'use client';
 
-import { useFormStatus } from "react-dom";
-import styles from "./Buttons.module.css";
+import Image from 'next/image';
+import { useFormStatus } from 'react-dom';
+import styles from './Buttons.module.css';
 
 export function FormButton({ text, pendingText, type }) {
   const { pending } = useFormStatus();
 
   return (
-    <button type={type} disabled={pending} className={styles.text}>
+    <button type={type} disabled={pending} className={styles.formButton}>
       {pending ? pendingText : text}
+    </button>
+  );
+}
+
+export function EyeButton({ action, showPassword, isPending }) {
+  return (
+    <button
+      type="button"
+      onClick={action}
+      className={styles.passwordToggle}
+      aria-label={showPassword ? 'Hide password' : 'Show password'}
+      disabled={isPending}
+    >
+      <Image
+        src={
+          showPassword ? '/assets/eye-closed-icon.svg' : '/assets/eye-icon.svg'
+        }
+        width={20}
+        height={14}
+        alt={showPassword ? 'Hidden' : 'Visible'}
+        className={styles.toggleIcon}
+      />
     </button>
   );
 }
--- frontend/src/components/forms/LoginForm.jsx
+++ frontend/src/components/forms/LoginForm.jsx
@@ -1,23 +1,32 @@
-"use client";
+'use client';
 
-import Form from "next/form";
-import { redirect } from "next/navigation";
-import { useActionState } from "react";
-import { loginAction } from "@/actions/authActions";
-import { FormButton } from "@/components/buttons/Buttons";
-import { DEFAULT_LOGIN_REDIRECT } from "@/route";
-import styles from "./LoginForm.module.css";
+import Form from 'next/form';
+import { useState } from 'react';
+import { useActionState } from 'react';
+import { redirect } from 'next/navigation';
+import { loginAction } from '@/actions/authActions';
+import { FormButton } from '@/components/buttons/Buttons';
+import { DEFAULT_LOGIN_REDIRECT } from '@/route';
+import { EyeButton } from '@/components/buttons/Buttons';
+import styles from './LoginForm.module.css';
 
 const initialState = {
   errors: {},
-  success: "",
+  success: '',
+  formEmail: '',
 };
 
 export default function LoginForm() {
   const [state, formAction, isPending] = useActionState(
     loginAction,
-    initialState,
+    initialState
   );
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
 
   if (state.success) {
     redirect(DEFAULT_LOGIN_REDIRECT);
@@ -25,43 +34,54 @@ export default function LoginForm() {
 
   return (
     <Form action={formAction} className={styles.form}>
-      <h2 className={styles.title}>Welcome!</h2>
+      <h1 className={styles.title1}>Estate</h1>
+      <div className={styles.mainContainer}>
+        <h2 className={styles.title2}>Welcome!</h2>
 
-      {Object.keys(state.errors).length > 0 && state.errors.general && (
-        <div className={styles.generalError}>{state.errors.general}</div>
-      )}
-      {state.success && <div className={styles.success}>{state.success}</div>}
-
-      <div className={styles.inputContainer}>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          placeholder="Email"
-          disabled={isPending}
-          className={styles.input}
-        />
-        {Object.keys(state.errors).length > 0 && state.errors.email && (
-          <div className={styles.error}>{state.errors.email}</div>
+        {Object.keys(state.errors).length > 0 && state.errors.general && (
+          <div className={styles.generalError}>{state.errors.general}</div>
         )}
-      </div>
+        {state.success && <div className={styles.success}>{state.success}</div>}
 
-      <div className={styles.inputContainer}>
-        <input
-          id="password"
-          name="password"
-          type="password"
-          placeholder="Password"
-          disabled={isPending}
-          className={styles.input}
-        />
-        {Object.keys(state.errors).length > 0 && state.errors.password && (
-          <div className={styles.error}>{state.errors.password}</div>
-        )}
-      </div>
+        <div className={styles.formInputContainer}>
+          <div className={styles.inputContainer}>
+            <input
+              id="email"
+              name="email"
+              placeholder="Email"
+              disabled={isPending}
+              defaultValue={state.formEmail}
+              className={styles.input}
+            />
+            {Object.keys(state.errors).length > 0 && state.errors.email && (
+              <div className={styles.error}>{state.errors.email}</div>
+            )}
+          </div>
 
-      <div className={styles.buttonContainer}>
-        <FormButton text="Login" pendingText="Logging in..." type="submit" />
+          <div className={styles.inputContainer}>
+            <div className={styles.passwordContainer}>
+              <input
+                id="password"
+                name="password"
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Password"
+                disabled={isPending}
+                className={styles.input}
+              />
+              <EyeButton
+                action={togglePasswordVisibility}
+                showPassword={showPassword}
+                isPending={isPending}
+              />
+            </div>
+            {Object.keys(state.errors).length > 0 && state.errors.password && (
+              <div className={styles.error}>{state.errors.password}</div>
+            )}
+          </div>
+        </div>
+        <div className={styles.buttonContainer}>
+          <FormButton text="Login" pendingText="Logging in..." type="submit" />
+        </div>
       </div>
     </Form>
   );
