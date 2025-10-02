7c18ee7e192aa0f1f1dd2791fa95d2a12d13e263
sahil-iftekhar <mailsahiliftekhar@gmail.com>
2025-10-02 21:45:33 +0600

    Login,Signup and Signup page blur fixed
    Co-authored-by: Sabiq Zahid <sabiqzahid@users.noreply.github.com>

--- frontend/src/app/auth/signup/page.jsx
+++ frontend/src/app/auth/signup/page.jsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 import styles from "@/styles/SignUpPage.module.css";
 
 export default function SignUpPage() {
-  const imgUrl="/real-estate/real-estate.jpg";
-  return( 
+  const imgUrl = "/real-estate/real-estate.jpg";
+  return (
     <div className={styles.background}>
       <Image src={imgUrl} alt="background" fill priority />
       <div className={styles.signUpPageCardContainer}>
         <SignUpPageCard />
       </div>
     </div>
-  )
+  );
 }
