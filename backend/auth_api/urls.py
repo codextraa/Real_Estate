108c607a7171014994a92baaf08a0bf3fe45647d
waseesun <revengersun@example.com>
2025-10-03 20:24:34 +0600

    Refresh View, urls and signup form fixed

--- backend/auth_api/urls.py
+++ backend/auth_api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
+    path("refresh-token/", views.RefreshTokenView.as_view(), name="refresh-token"),
 ]
