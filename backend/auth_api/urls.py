484827cd0d6ec44b00ce6940ac28ca2456bacaa1
Ariana Suha <arianahoque94@gmail.com>
2025-10-05 22:03:27 +0600

    Added UserListSerializer, UserRetrieveSerializer and made permission and serializer class methods in views.py and updated urls

--- backend/auth_api/urls.py
+++ backend/auth_api/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r"user", views.UserViewSet)
+
 urlpatterns = [
+    path("", include(router.urls)),
     path("login/", views.LoginView.as_view(), name="login"),
     path("refresh-token/", views.RefreshTokenView.as_view(), name="refresh-token"),
 ]
