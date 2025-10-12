7812eb6a66781735533c29ef2c2edd9dfccb20d5
Sabiq Zahid <sabiqzahid11@gmail.com>
2025-10-13 01:07:13 +0600

    userProfileAction almost done

--- frontend/src/components/forms/ProfileForm.jsx
+++ frontend/src/components/forms/ProfileForm.jsx
@@ -1,17 +1,37 @@
 import Form from "next/form";
 import { FormButton } from "@/components/buttons/Buttons";
 import styles from "./ProfileForm.module.css";
+import { updateUserAction } from "@/actions/userActions";
+import { useActionState } from "react";
 
 export default function ProfileForm({ userData }) {
+  const initialState = {
+    errors: {},
+    success: "",
+    formEmailAddress: "",
+    formFirstName: "",
+    formLastName: "",
+    formUsername: "",
+  };
+
+  const [state, formActions, isPending] = useActionState(
+    updateUserAction.bind(userData.id, null, userData),
+    initialState,
+  );
+
+  // error handling baki
+
   return (
-    <Form action={userData} className={styles.Form}>
+    <Form action={formActions} className={styles.Form}>
       <div className={styles.inputFormContainer}>
         <div className={styles.inputContainer}>
           <input
             type="email"
-            id="email_address"
-            name="email_address"
+            id="email"
+            name="email"
             placeholder="Email Address"
+            disabled={isPending}
+            defaultValue={state.formEmail}
             className={styles.input}
           />
         </div>
@@ -21,6 +41,8 @@ export default function ProfileForm({ userData }) {
             id="first_name"
             name="first_name"
             placeholder="First Name"
+            disabled={isPending}
+            defaultValue={state.formFirstName}
             className={styles.input}
           />
         </div>
@@ -30,6 +52,8 @@ export default function ProfileForm({ userData }) {
             id="last_name"
             name="last_name"
             placeholder="Last Name"
+            disabled={isPending}
+            defaultValue={state.formLastName}
             className={styles.input}
           />
         </div>
@@ -39,6 +63,8 @@ export default function ProfileForm({ userData }) {
             id="username"
             name="username"
             placeholder="Username"
+            disabled={isPending}
+            defaultValue={state.formUsername}
             className={styles.input}
           />
         </div>
