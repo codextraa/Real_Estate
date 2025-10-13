2b3978a988eb966b1de66379dd036bf278f84336
Ariana Suha <arianahoque94@gmail.com>
2025-10-13 22:57:27 +0600

    Added urls for property and added create method in AgentViewSet for Agent

--- backend/auth_api/views.py
+++ backend/auth_api/views.py
@@ -21,7 +21,66 @@ from .serializers import (
     UserSerializer,
     UserListSerializer,
     UserRetrieveSerializer,
+    AgentSerializer,
 )
+
+
+def check_request_data(request):
+    """Check if request data is valid."""
+
+    current_user = request.user
+    if "is_superuser" in request.data:
+        return Response(
+            {
+                "error": "You do not have permission to create a superuser. Contact Developer."
+            },
+            status=status.HTTP_403_FORBIDDEN,
+        )
+
+    if "is_staff" in request.data and not current_user.is_superuser:
+        return Response(
+            {"error": "You do not have permission to create an admin user."},
+            status=status.HTTP_403_FORBIDDEN,
+        )
+
+    if "slug" in request.data or "is_active" in request.data:
+        return Response(
+            {"error": "Forbidden fields cannot be updated."},
+            status=status.HTTP_403_FORBIDDEN,
+        )
+
+    password = request.data.get("password")
+    if not request.data.get("c_password"):
+        return Response(
+            {"error": "Please confirm your password."},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    c_password = request.data.pop("c_password")
+    if password != c_password:
+        return Response(
+            {"error": "Passwords do not match"}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    return current_user
+
+
+def get_user_role(user):
+    """Get user role."""
+    user_groups = user.groups.all()
+
+    if user_groups.filter(name="Default").exists():
+        user_role = "Default"
+    elif user_groups.filter(name="Agent").exists():
+        user_role = "Agent"
+    elif user_groups.filter(name="Admin").exists():
+        user_role = "Admin"
+    elif user_groups.filter(name="Superuser").exists():
+        user_role = "Superuser"
+    else:
+        user_role = "UnAuthorized"
+
+    return user_role
 
 
 def check_user_validity(email):
@@ -42,24 +101,6 @@ def check_user_validity(email):
         )
 
     return user
-
-
-def get_user_role(user):
-    """Get user role."""
-    user_groups = user.groups.all()
-
-    if user_groups.filter(name="Default").exists():
-        user_role = "Default"
-    elif user_groups.filter(name="Agent").exists():
-        user_role = "Agent"
-    elif user_groups.filter(name="Admin").exists():
-        user_role = "Admin"
-    elif user_groups.filter(name="Superuser").exists():
-        user_role = "Superuser"
-    else:
-        user_role = "UnAuthorized"
-
-    return user_role
 
 
 def check_user_id(user_id):
@@ -250,40 +291,11 @@ class UserViewSet(ModelViewSet):
 
     def create(self, request, *args, **kwargs):  # pylint: disable=R0911
         """Create new user and send email verification link."""
-        current_user = self.request.user
 
-        if "is_superuser" in request.data:
-            return Response(
-                {
-                    "error": "You do not have permission to create a superuser. Contact Developer."
-                },
-                status=status.HTTP_403_FORBIDDEN,
-            )
+        check_integrity = check_request_data(request)
 
-        if "is_staff" in request.data and not current_user.is_superuser:
-            return Response(
-                {"error": "You do not have permission to create an admin user."},
-                status=status.HTTP_403_FORBIDDEN,
-            )
-
-        if "slug" in request.data or "is_active" in request.data:
-            return Response(
-                {"error": "Forbidden fields cannot be updated."},
-                status=status.HTTP_403_FORBIDDEN,
-            )
-
-        password = request.data.get("password")
-        if not request.data.get("c_password"):
-            return Response(
-                {"error": "Please confirm your password."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        c_password = request.data.pop("c_password")
-        if password != c_password:
-            return Response(
-                {"error": "Passwords do not match"}, status=status.HTTP_400_BAD_REQUEST
-            )
+        if isinstance(check_integrity, Response):
+            return check_integrity
 
         response = super().create(request, *args, **kwargs)
 
@@ -369,3 +381,82 @@ class UserViewSet(ModelViewSet):
             )
 
         return response
+
+
+class AgentViewSet(ModelViewSet):
+    """Agent Viewset."""
+
+    queryset = get_user_model().objects.all()
+    serializer_class = AgentSerializer
+    renderer_classes = [ViewRenderer]
+    authentication_classes = [JWTAuthentication]  # Using jwtoken
+    filter_backends = [DjangoFilterBackend]
+    http_method_names = ["get", "post", "patch", "delete"]
+
+    def get_permissions(self):
+        """Assign permissions based on action."""
+        if self.action == "create":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    def get_queryset(self):  # pylint: disable=R0911
+        """Queryset for User View."""
+        if self.action == "list":
+            if self.request.user.is_staff:
+                return get_user_model().objects.all()
+            return get_user_model().objects.none()
+
+        if self.action == "retrieve":
+            if self.request.user.is_staff:
+                return get_user_model().objects.all()
+            if self.request.user.is_agent:
+                return get_user_model().objects.filter(is_staff=False)
+
+            allowed_user_queryset = Q(pk=self.request.user.pk) | Q(is_agent=True)
+            return get_user_model().objects.filter(allowed_user_queryset)
+
+        # create, update, partial_update, destroy
+        if self.request.user.is_superuser:
+            return get_user_model().objects.all()
+        return get_user_model().objects.filter(pk=self.request.user.pk)
+
+    def http_method_not_allowed(self, request, *args, **kwargs):
+        return http_method_mixin(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """Create Agent Profile"""
+
+        check_integrity = check_request_data(request)
+
+        if isinstance(check_integrity, Response):
+            return check_integrity
+
+        request_data = request.data.copy()
+        agent_keys = ["company_name", "bio", "image_url"]
+        user_request_data = request_data
+        agent_request_data = {}
+
+        for key in user_request_data.keys():
+            if key in agent_keys:
+                agent_request_data[key] = request_data[key]
+                user_request_data.pop(key)
+
+        user_serializer = UserSerializer(data=user_request_data)
+        user_serializer.is_valid(raise_exception=True)
+        user = user_serializer.save()
+
+        agent_request_data["user"] = user
+        agent_serializer = self.get_serializer(data=agent_request_data)
+        agent_serializer.is_valid(raise_exception=True)
+        agent = agent_serializer.save()
+        agent.image_url = "profile_images/default_profile.jpg"
+        agent.save()
+
+        return Response(
+            {
+                "success": "Agent created successfully",
+            },
+            status=status.HTTP_201_CREATED,
+        )
