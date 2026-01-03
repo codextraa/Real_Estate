c75ac3178da8904f0a34282a213ded86c2ef732b
Wasee Sun <revengersun@gmail.com>
2026-01-03 19:06:54 +0600

    Added New AI api url

--- frontend/src/libs/api.js
+++ frontend/src/libs/api.js
@@ -5,7 +5,11 @@ const HTTPS = process.env.HTTPS === "true";
 const API_URL = HTTPS
   ? process.env.API_BASE_HTTPS_URL
   : process.env.API_BASE_URL;
+const API_AI_URL = HTTPS
+  ? process.env.API_AI_BASE_HTTPS_URL
+  : process.env.API_AI_BASE_URL;
 const apiClient = new ApiClient(API_URL);
+const apiAiClient = new ApiClient(API_AI_URL);
 
 export const refreshToken = async (refreshToken) => {
   return await apiClient.post("/auth-api/refresh-token/", {
