d223a93cab75213e81d904d211ec7d0c17603c17
Wasee Sun <revengersun@gmail.com>
2026-05-31 18:22:17 +0600

    Added whitenoise and turned on ai workflows

--- backend_ai/backend_ai/settings.py
+++ backend_ai/backend_ai/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -145,7 +146,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static_ai/"
-STATIC_ROOT = "/app/static_ai"
+
+if DJANGO_ENV == "development":
+    STATIC_ROOT = "/app/static_ai"
+else:
+    STATIC_ROOT = "/home/django_ai/app/static_ai"
+    STORAGES = {
+        "default": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
